7e1b011e71b46b19b31f3a82e6cbe945d866ed82
stanley <stanley2001@live.nl>
2018-11-14 09:29:46 +0100

    Exercise 4 done.

---
+++ src/Opdracht4/javascript1.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerHTML = "Dit is de paginatitel";
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+document.getElementById("aap1").src = "img/aap1.jpg";
+document.getElementById("aap2").src = "img/aap2.jpg";
