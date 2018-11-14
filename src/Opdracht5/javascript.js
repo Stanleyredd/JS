edc4288317d80c494ac9b8bbfae538914360e658
stanley <stanley2001@live.nl>
2018-11-14 13:11:11 +0100

    Exercise 5 done.

---
+++ src/Opdracht5/javascript.js
@@ -0,0 +1,11 @@
+/*Opdracht 5*/
+
+document.getElementById("bl2").style.marginTop = "15px";
+document.getElementById("bl3").style.marginTop = "40px";
+
+document.getElementById("bl1").style.backgroundColor = "cyan";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+
+
