f4c06f655cd7790fad092d0ae2cce47538b1b84d
stanley <stanley2001@live.nl>
2018-11-15 11:50:48 +0100

    little changes.

--- src/Opdracht4/javascript.js
+++ src/Opdracht4/javascript.js
@@ -1,3 +1,4 @@
+/*Opdracht 4*/
 document.getElementById("paginatitel").innerHTML = "Dit is de paginatitel";
 document.getElementById("subkop").innerHTML = "Dit is de subkop";
 document.getElementById("aap1").src = "img/aap1.jpg";
